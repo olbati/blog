e375c4911af31fee933038f528253019b8eb69be
Nizar GARRACHE <nizar.garrache@olbati.com>
2015-06-16 16:28:06 +0200

    Update ABSPATH to wp-core path.

--- wp-config.php
+++ wp-config.php
@@ -74,7 +74,7 @@ define('WP_DEBUG', false);
 
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+	define('ABSPATH', dirname(__FILE__) . '/wp-core');
 
 /** Sets up WordPress vars and included files. */
-require_once(ABSPATH . 'wp-settings.php');
+require_once(ABSPATH . 'wp-core/wp-settings.php');
