255bf653726d68fcce3151b80513c1c7dfe42e16
Nizar GARRACHE <nizar.garrache@olbati.com>
2016-04-18 14:07:18 +0200

    Update footer content.

--- wp-content/themes/business/footer.php
+++ wp-content/themes/business/footer.php
@@ -11,12 +11,12 @@
 	</section><!-- #content -->
     
 	<footer id="colophon" class="site-footer" role="contentinfo"> 
-    	<div class="grid grid-pad">
-    		 <div class="col-1-3"><b>Contact:</b> <br>154 rue du Faubourg Saint-Antoine <br>75012 Paris, France</div>
+        <div class="grid grid-pad">
+            <div class="col-1-3">We are passionate Software Craftsmans adding value to business. We keep learning and improving our skills to provide the best to our clients. Some of our passions are Java & Web development, Agile, Craftsmanship, DevOps and Big Data.</div>
 			 
-			  <div class="col-1-3"><b>General information:</b><br> contact@olbati.com<br>Employment: hr@olbati.com</div>
+	        <div class="col-1-3"><b>OLBATI</b><br/>14 rue des Immeubles Industriels<br/>75011 Paris<br/>contact@olbati.com<br/>Phone: +33 (0) 9 54 14 29 43<br/>Fax: +33 (0) 9 57 56 74 82</div>
 			 
-			  <div class="col-1-3"><div>Phone: +33 (0)9 51 24 72 00<br>Fax: +33 (0)9 56 24 72 00</div><div><?php echo business_media_icons(); ?></div></div>
+			<div class="col-1-3"><div><?php echo business_media_icons(); ?></div>
 			 
 			<?php if ( is_active_sidebar('footer_column_1') ) : ?>
                 <div class="col-1-4">
